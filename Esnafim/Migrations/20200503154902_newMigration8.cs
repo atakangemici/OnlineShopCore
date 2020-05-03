1bd7722f6725c2c969c1fcf78ffd129864eee92f
Atakan Gemici <atakan.gemici@ofisim.com>
2020-05-03 19:15:26 +0300

    publish commit

---
+++ Esnafim/Migrations/20200503154902_newMigration8.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Esnafim.Migrations
+{
+    public partial class newMigration8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
